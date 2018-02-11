a431dae924caac3306282e68571aa7476e0ec8bc
Alex Gong <alexgong1995@gmail.com>
2018-02-10 20:06:55 -0500

    waiting luke to debug, add classes to MyApplication

--- src/mf/rest/MyApplication.java
+++ src/mf/rest/MyApplication.java
@@ -18,6 +18,9 @@ public class MyApplication extends Application {
         classes.add(SellFundAction.class);
         classes.add(BuyFundAction.class);
         classes.add(CreateFundAction.class);
+        classes.add(CreateCustomerAccountAction.class);
+        classes.add(LogoutAction.class);
+        classes.add(TransitionDayAction.class);
         new Model();
         return classes;
     }
