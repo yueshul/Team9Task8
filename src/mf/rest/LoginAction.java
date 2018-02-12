5b02fe0805df95bf1b1f22fef9e918224a3ac5fe
Alex Gong <alexgong1995@gmail.com>
2018-02-12 16:02:21 -0500

    fix request errors in transition day and logout action. (No longer use fund price history bean)

--- src/mf/rest/LoginAction.java
+++ src/mf/rest/LoginAction.java
@@ -2,7 +2,6 @@ package mf.rest;
 
 import java.util.HashMap;
 import java.util.Map;
-
 import javax.json.JsonObject;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -12,13 +11,8 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-
 import org.genericdao.MatchArg;
 import org.genericdao.RollbackException;
-
-import com.google.gson.Gson;
-import mf.rest.ResponseMessage;
-
 import mf.databean.CustomerBean;
 import mf.databean.EmployeeBean;
 import mf.databean.FundBean;
@@ -57,10 +51,8 @@ public class LoginAction{
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     public Response loginAction(JsonObject object,@Context HttpServletRequest request) {
-        //Gson gson = new Gson();
         if(model == null)init();
         String userName = object.get("username").toString().replaceAll("\"", "");
-        //String type = object.get("type").toString().replaceAll("\"", "");
         String password = object.get("password").toString().replaceAll("\"", "");
         String successMessage = "Welcome "+userName;
         String failMessage = "There seems to be an issue with the username/password combination that you entered";
@@ -69,11 +61,6 @@ public class LoginAction{
         try {
             EmployeeBean employee = null;
             CustomerBean customer = null;
-//            if(type.equals("Customer")) {
-//                customer = customerDAO.read(userName);
-//            }else if(type.equals("Employee")) {
-//                employee = employeeDAO.read(userName);
-//            }
             if (employeeDAO.read(userName) != null) {
                 employee = employeeDAO.read(userName);
             		if (employee.getPassword().equals(password)) {
--- src/mf/rest/TransitionDayAction.java
+++ src/mf/rest/TransitionDayAction.java
@@ -4,31 +4,28 @@ import javax.servlet.http.HttpSession;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import org.genericdao.RollbackException;
 import org.genericdao.Transaction;
 import mf.databean.FundBean;
-import mf.databean.FundPriceHistoryBean;
 import mf.model.FundDAO;
-import mf.model.FundPriceHistoryDAO;
 import mf.model.Model;
 
 @Path("/transitionDay")
 public class TransitionDayAction {
     private Model model;
     private FundDAO fundDAO;
-    private FundPriceHistoryDAO fundPriceHistoryDAO;
     
     public void init() {
         model = new Model();
         this.fundDAO = model.getFundDAO();
-        this.fundPriceHistoryDAO = model.getFundPriceHistoryDAO();
     }
     
     @POST
     @Produces(MediaType.APPLICATION_JSON)
-    public Response performPost(HttpServletRequest request) {
+    public Response performPost(@Context HttpServletRequest request) {
         if(model == null)init();
         String successMessage = "The fund prices have been successfully recalculated";
         String notLoggedInMessage = "You are not currently logged in";
@@ -45,24 +42,14 @@ public class TransitionDayAction {
         }
         try {
             Transaction.begin();
-            FundPriceHistoryBean bean = new FundPriceHistoryBean();
             FundBean[] funds = fundDAO.match();
             for(FundBean fund:funds) {
-                int fundId = fund.getFundId();
-                double oldPrice = fund.getLatestPrice();
-                double upper = oldPrice * 1.1;
-                double lower = oldPrice * 0.9;
-                double gap = upper - lower;
-                double newPrice = Math.random() * gap + lower;
-                bean.setFundId(fundId);
-                bean.setPrice(newPrice);
-                fundPriceHistoryDAO.create(bean);
-                fund.setLatestPrice(newPrice);
+                fund.setLatestPrice(generateNewPrice(fund));
                 fundDAO.update(fund);
             }
             Transaction.commit();
         } catch (RollbackException e) {
-            String rollBackMessage = "Transaction roll back";
+            String rollBackMessage = "Transaction roll back"+e.getMessage();
             message.setMessage(rollBackMessage);
             return Response.status(403).entity(message).build();
         } finally {
@@ -70,5 +57,13 @@ public class TransitionDayAction {
         }
         message.setMessage(successMessage);
         return Response.status(200).entity(message).build();
+    }
+    
+    public double generateNewPrice(FundBean fund) {
+        double oldPrice = fund.getLatestPrice();
+        double upper = oldPrice * 1.1;
+        double lower = oldPrice * 0.9;
+        double gap = upper - lower;
+        return Math.random() * gap + lower;
     }
 }
