8a3ecf92cee959ee243e535b2bb459b1a99b777a
klu2 <klu2@andrew.cmu.edu>
2018-02-20 11:47:50 -0500

    fsdfasdczx

--- src/mf/rest/CreateCustomerAccountAction.java
+++ src/mf/rest/CreateCustomerAccountAction.java
@@ -76,71 +76,7 @@ public class CreateCustomerAccountAction {
         String successMessage = first_name+" was registered successfully";
         String notLoggedInMessage = "You are not currently logged in";
         String notAdminMessage = "You must be an employee to perform this action";
-        
-        String []states = {"Alaska","Alabama", "Arkansas",  "American Samoa",
-                "Arizona","California", "Colorado", "Connecticut", "District of Columbia",
-                "Delaware","Florida","Georgia","Guam", "Hawaii","Iowa", "Idaho", "Illinois",
-                "Indiana",  "Kansas",   "Kentucky",  "Louisiana", "Massachusetts",
-                "Maryland", "Maine", "Michigan",
-                "Minnesota", "Missouri",
-                "Mississippi",
-                "Montana",
-                "North Carolina", "North Dakota",
-                "Nebraska",
-                "New Hampshire",
-                "New Jersey",
-                "New Mexico",
-                "Nevada",
-                "New York",
-                "Ohio",
-                "Oklahoma",
-                "Oregon",
-                "Pennsylvania",
-                "Puerto Rico",
-                "Rhode Island",
-                "South Carolina",
-                "South Dakota",
-                "Tennessee",
-                "Texas",
-                "Utah",
-                "Virginia",
-                "Virgin Islands",
-                "Vermont",
-                "Washington",
-                "Wisconsin",
-                "West Virginia",
-                "Wyoming","AK",
-                "AL",  "AR",  "AS", "AZ", "CA","CO",
-                "CT", "DC", "DE",
-                "FL",
-                "GA",
-                "GU",
-                "HI",
-                "IA",
-                "ID",
-                "IL",
-                "IN",
-                "KS",
-                "KY",
-                "LA",
-                "MA",
-                "MD",
-                "ME",
-                "MI",
-                "MN",
-                "MO",
-                "MS",
-                "MT",
-                "NC",
-                "ND",
-                "NE",
-                "NH",
-                "NJ",
-                "NM",
-                "NV",
-                "NY",
-                "OH", "OK", "OR", "PA",
-                "PR", "RI", "SC",  "SD",  "TN",  "TX",  "UT", "VA", "VI", "VT", "WA","WI","WV","WY"};
+   
         
         if (session.getAttribute("customer")!=null && session.getAttribute("employee") == null) {
             message.setMessage(notAdminMessage);
@@ -150,10 +86,7 @@ public class CreateCustomerAccountAction {
             message.setMessage(notLoggedInMessage);
             return Response.status(200).entity(message).build();
         }
-        if (!Arrays.asList(states).contains(state)||!zip.matches("[a-zA-Z0-9]*")) {
-            message.setMessage(duplicateMessage);
-            return Response.status(200).entity(message).build();
-        }
+       
         try {
             Transaction.begin();
             synchronized (customerDAO) {
